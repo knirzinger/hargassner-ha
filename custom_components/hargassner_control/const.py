9206dbe55801e5ee435c9abbe3a56e5b38efe57e
knirzinger <ronald@knirzinger.com>
2026-03-01 18:41:03 +0100

    Initial release v0.1.0

---
+++ custom_components/hargassner_control/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Hargassner Control integration."""
+
+DOMAIN = "hargassner_control"
+
+# Config entry keys
+CONF_INSTALLATION_ID = "installation_id"
+
+# Coordinator update interval
+SCAN_INTERVAL_MINUTES = 15
